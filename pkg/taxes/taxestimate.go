16e464604f17842fb00a89d946a2db090667a43a
Matt Fenwick <mfenwick100@gmail.com>
2024-07-22 12:22:32 -0400

    clean up totals

--- pkg/taxes/taxestimate.go
+++ pkg/taxes/taxestimate.go
@@ -97,98 +97,77 @@ func (e *TaxEstimate) PrettyPrint() {
 	}
 	fmt.Printf("income/input: \n%s\n\n", inputTable.ToFormattedTable())
 
-	// ordinary tax
-	ordinaryTaxTable := NewTable([]string{"Range", "Rate", "Taxable amount", "Tax"})
-	for _, t := range e.OrdinaryTaxes {
-		end := "<none>"
-		if t.Bracket.End != nil {
-			end = fmt.Sprintf("%6d", *t.Bracket.End)
-		}
-		ordinaryTaxTable.AddRow([]string{
-			fmt.Sprintf("%6d - %s", t.Bracket.Start, end),
-			fmt.Sprintf("%.0f%%", t.Bracket.RawBracket.Rate.ToDebugPercentage()),
-			fmt.Sprintf("%d", t.BracketTax.TaxableAmount),
-			fmt.Sprintf("%d", t.BracketTax.Tax),
-		})
-	}
-	fmt.Printf("ordinary tax:\n%s\n\n", ordinaryTaxTable.ToFormattedTable())
+	// taxes
+	taxTable := NewTable([]string{"Description", "Rate (%)", "Taxable amount", "Tax"})
+	taxTotalTable := NewTable([]string{"Description", "Tax"})
 
-	// medicare
-	medicareTable := NewTable([]string{"", "Taxable amount", "Rate", "Tax"},
-		[]string{"Base wage",
-			intToString(e.Medicare.BaseWageIncome),
-			fmt.Sprintf("%.2f", e.TaxYearConstants.MedicareBaseRate.ToDebugPercentage()),
-			intToString(e.Medicare.BaseWageTax)},
-		[]string{"Addnl wage",
-			intToString(e.Medicare.AdditionalWageIncome),
-			fmt.Sprintf("%.2f", e.TaxYearConstants.MedicareAdditionalRate.ToDebugPercentage()),
-			intToString(e.Medicare.AdditionalWageTax)},
-		[]string{"NIIT",
-			intToString(e.Medicare.NiitIncome),
-			fmt.Sprintf("%.2f", e.TaxYearConstants.NetInvestmentTaxRate.ToDebugPercentage()),
-			intToString(e.Medicare.NiitTax)},
-	)
-	// fmt.Printf(" - threshold: %d\n", -1) // TODO ???
-	fmt.Printf("medicare: \n%s\n\n", medicareTable.ToFormattedTable())
+	// medicare tax
+	taxTable.AddRow([]string{"Medicare base wage",
+		fmt.Sprintf("%.2f", e.TaxYearConstants.MedicareBaseRate.ToDebugPercentage()),
+		intToString(e.Medicare.BaseWageIncome),
+		intToString(e.Medicare.BaseWageTax)})
+	taxTable.AddRow([]string{"Medicare addnl wage",
+		fmt.Sprintf("%.2f", e.TaxYearConstants.MedicareAdditionalRate.ToDebugPercentage()),
+		intToString(e.Medicare.AdditionalWageIncome),
+		intToString(e.Medicare.AdditionalWageTax)})
+	taxTable.AddRow([]string{"Medicare NIIT",
+		fmt.Sprintf("%.2f", e.TaxYearConstants.NetInvestmentTaxRate.ToDebugPercentage()),
+		intToString(e.Medicare.NiitIncome),
+		intToString(e.Medicare.NiitTax)})
 
-	// social security
-	socialSecurityTable := NewTable([]string{"Job", "Income", "Taxable amount", "Tax"})
+	// social security tax
+	ssTotal := int64(0)
 	for _, t := range e.SocialSecurity {
-		socialSecurityTable.AddRow([]string{
-			t.Description,
-			fmt.Sprintf("%d", t.WageIncome),
-			fmt.Sprintf("%d", t.TaxableAmount),
-			fmt.Sprintf("%d", t.Tax),
+		taxTable.AddRow([]string{
+			fmt.Sprintf("Social security %s", t.Description),
+			fmt.Sprintf("%.1f", e.TaxYearConstants.SocialSecurityRate.ToDebugPercentage()),
+			intToString(t.TaxableAmount),
+			intToString(t.Tax),
 		})
+		ssTotal += t.Tax
 	}
-	fmt.Printf("social security tax:\n%s\n", socialSecurityTable.ToFormattedTable())
 
-	// LTCG
-	ltcgTaxTable := NewTable([]string{"Range", "Rate", "Taxable amount", "Tax"})
+	// ordinary tax
+	ordinaryTotal := int64(0)
+	for _, t := range e.OrdinaryTaxes {
+		end := "<none>"
+		if t.Bracket.End != nil {
+			end = fmt.Sprintf("%6d", *t.Bracket.End)
+		}
+		taxTable.AddRow([]string{
+			fmt.Sprintf("Ordinary: %6d - %s", t.Bracket.Start, end),
+			fmt.Sprintf("%.0f", t.Bracket.RawBracket.Rate.ToDebugPercentage()),
+			fmt.Sprintf("%d", t.BracketTax.TaxableAmount),
+			fmt.Sprintf("%d", t.BracketTax.Tax),
+		})
+		ordinaryTotal += t.BracketTax.Tax
+	}
+
+	// LTCG tax
+	ltcgTotal := int64(0)
 	for _, t := range e.LTCGTaxes {
 		end := "<none>"
 		if t.Bracket.End != nil {
 			end = fmt.Sprintf("%6d", *t.Bracket.End)
 		}
-		ltcgTaxTable.AddRow([]string{
-			fmt.Sprintf("%6d - %s", t.Bracket.Start, end),
-			fmt.Sprintf("%.0f%%", t.Bracket.RawBracket.Rate.ToDebugPercentage()),
+		taxTable.AddRow([]string{
+			fmt.Sprintf("LTCG: %6d - %s", t.Bracket.Start, end),
+			fmt.Sprintf("%.0f", t.Bracket.RawBracket.Rate.ToDebugPercentage()),
 			fmt.Sprintf("%d", t.BracketTax.TaxableAmount),
 			fmt.Sprintf("%d", t.BracketTax.Tax),
 		})
+		ltcgTotal += t.BracketTax.Tax
 	}
-	fmt.Printf("ltcg tax:\n%s\n\n", ltcgTaxTable.ToFormattedTable())
 
-	// totals
-	totalsTable := NewTable([]string{"Description", "Tax"})
-	for _, t := range e.OrdinaryTaxes {
-		totalsTable.AddRow([]string{
-			fmt.Sprintf("Wage+STCG: %.0f%%", t.Bracket.RawBracket.Rate.ToDebugPercentage()),
-			intToString(t.BracketTax.Tax),
-		})
-	}
-	totalsTable.AddRow([]string{"Wage+STCG total",
-		intToString(slice.Sum(slice.Map(func(t *TaxEstimateBracket) int64 { return t.BracketTax.Tax }, e.OrdinaryTaxes)))})
-	for _, t := range e.LTCGTaxes {
-		totalsTable.AddRow([]string{
-			fmt.Sprintf("LTCG: %.0f%%", t.Bracket.RawBracket.Rate.ToDebugPercentage()),
-			intToString(t.BracketTax.Tax),
-		})
-	}
-	totalsTable.AddRow([]string{"LTCG total",
-		intToString(slice.Sum(slice.Map(func(t *TaxEstimateBracket) int64 { return t.BracketTax.Tax }, e.LTCGTaxes)))})
-	totalsTable.AddRow([]string{"Medicare base wage", intToString(e.Medicare.BaseWageTax)})
-	totalsTable.AddRow([]string{"Medicare addnl wage", intToString(e.Medicare.AdditionalWageTax)})
-	totalsTable.AddRow([]string{"Medicare NIIT", intToString(e.Medicare.NiitTax)})
-	totalsTable.AddRow([]string{"Medicare total",
-		intToString(e.Medicare.BaseWageTax + e.Medicare.AdditionalWageTax + e.Medicare.NiitTax)})
-	for _, s := range e.SocialSecurity {
-		totalsTable.AddRow([]string{"Social security " + s.Description, intToString(s.Tax)})
-	}
-	totalsTable.AddRow([]string{"Social security total",
-		intToString(slice.Sum(slice.Map(func(s *SocialSecurityTax) int64 { return s.Tax }, e.SocialSecurity)))})
+	medicareTotal := e.Medicare.BaseWageTax + e.Medicare.AdditionalWageTax + e.Medicare.NiitTax
+	taxTotalTable.AddRow([]string{"Medicare", intToString(medicareTotal)})
+	taxTotalTable.AddRow([]string{"Social security", intToString(ssTotal)})
+	taxTotalTable.AddRow([]string{"Ordinary", intToString(ordinaryTotal)})
+	taxTotalTable.AddRow([]string{"LTCG", intToString(ltcgTotal)})
+	taxTotalTable.AddRow([]string{"Grand total", intToString(medicareTotal + ssTotal + ordinaryTotal + ltcgTotal)})
 
-	fmt.Printf("totals:\n%s\n\n", totalsTable.ToFormattedTable())
+	fmt.Printf("tax breakdown:\n%s\n\n", taxTable.ToFormattedTable())
+	fmt.Printf("totals:\n%s\n\n", taxTotalTable.ToFormattedTable())
 
 	fmt.Printf("\n\n")
 }
