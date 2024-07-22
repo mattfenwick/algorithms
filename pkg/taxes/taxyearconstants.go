168eb3d9ce928d46e53e2ab2ffdb692505aa478e
Matt Fenwick <mfenwick100@gmail.com>
2024-07-22 10:17:37 -0400

    add 2023 tax constants

--- pkg/taxes/taxyearconstants.go
+++ pkg/taxes/taxyearconstants.go
@@ -26,6 +26,88 @@ type TaxYearConstants struct {
 }
 
 var (
+	TaxYear2023 = &TaxYearConstants{
+		SocialSecurityLimit:    160_200,
+		SocialSecurityRate:     &TaxRate{Rate: 62, Divisor: 1000},   // .062 = 6.2%
+		MedicareBaseRate:       &TaxRate{Rate: 145, Divisor: 10000}, // .0145 = 1.45%
+		MedicareAdditionalRate: &TaxRate{Rate: 9, Divisor: 1000},    // .009 = .9%
+		NetInvestmentTaxRate:   &TaxRate{Rate: 38, Divisor: 1000},   // .038 = 3.8%
+		ByStatus: map[FilingStatus]*TaxStatusConstants{
+			FilingStatusSingle: {
+				OrdinaryIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate10Percent, 11_000},
+					{Rate12Percent, 44_725},
+					{Rate22Percent, 95_375},
+					{Rate24Percent, 182_100},
+					{Rate32Percent, 231_250},
+					{Rate35Percent, 578_125},
+					{Rate37Percent, math.MaxInt64},
+				}),
+				LTCGIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate_0Percent, 44_625},
+					{Rate15Percent, 492_300},
+					{Rate20Percent, math.MaxInt64},
+				}),
+				StandardDeduction:           13_850,
+				MedicareAdditionalThreshold: 200_000,
+			},
+			FilingStatusMarriedJointly: {
+				OrdinaryIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate10Percent, 22_000},
+					{Rate12Percent, 89_450},
+					{Rate22Percent, 190_750},
+					{Rate24Percent, 364_200},
+					{Rate32Percent, 462_500},
+					{Rate35Percent, 693_750},
+					{Rate37Percent, math.MaxInt64},
+				}),
+				LTCGIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate_0Percent, 89_250},
+					{Rate15Percent, 553_850},
+					{Rate20Percent, math.MaxInt64},
+				}),
+				StandardDeduction:           27_700,
+				MedicareAdditionalThreshold: 250_000,
+			},
+			FilingStatusMarriedSeparately: {
+				OrdinaryIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate10Percent, 11_000},
+					{Rate12Percent, 44_725},
+					{Rate22Percent, 95_375},
+					{Rate24Percent, 182_100},
+					{Rate32Percent, 231_250},
+					{Rate35Percent, 346_875},
+					{Rate37Percent, math.MaxInt64},
+				}),
+				LTCGIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate_0Percent, 44_625},
+					{Rate15Percent, 276_900},
+					{Rate20Percent, math.MaxInt64},
+				}),
+				StandardDeduction:           13_850,
+				MedicareAdditionalThreshold: 125_000,
+			},
+			FilingStatusHeadOfHouseHold: {
+				OrdinaryIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate10Percent, 15_700},
+					{Rate12Percent, 59_63100850},
+					{Rate22Percent, 95_350},
+					{Rate24Percent, 182_100},
+					{Rate32Percent, 231_250},
+					{Rate35Percent, 578_100},
+					{Rate37Percent, math.MaxInt64},
+				}),
+				LTCGIncomeBrackets: NewStatusBrackets([]*RawBracket{
+					{Rate_0Percent, 59_750},
+					{Rate15Percent, 523_050},
+					{Rate20Percent, math.MaxInt64},
+				}),
+				StandardDeduction:           20_800,
+				MedicareAdditionalThreshold: 200_000,
+			},
+		},
+	}
+
 	TaxYear2024 = &TaxYearConstants{
 		SocialSecurityLimit:    168_600,
 		SocialSecurityRate:     &TaxRate{Rate: 62, Divisor: 1000},   // .062 = 6.2%
@@ -109,6 +191,7 @@ var (
 	}
 
 	TaxYears = map[int]*TaxYearConstants{
+		2023: TaxYear2023,
 		2024: TaxYear2024,
 	}
 )
