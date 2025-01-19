3e4ab5075152451e494c3a35384788facb79d515
Matt Fenwick <mfenwick100@gmail.com>
2025-01-19 16:22:52 -0500

    reorder keys

--- pkg/music/key.go
+++ pkg/music/key.go
@@ -74,20 +74,20 @@ func (k *Key) BluesScale() []*Note {
 }
 
 var Keys = []*Key{
-	{NewNote(NaturalG, 0, 1), 0, 6},
-	{NewNote(NaturalD, 0, 1), 0, 5},
-	{NewNote(NaturalA, 0, 1), 0, 4},
-	{NewNote(NaturalE, 0, 1), 0, 3},
-	{NewNote(NaturalB, 0, 1), 0, 2},
-	{NewNote(NaturalF, 0, 0), 0, 1},
 	{NewNote(NaturalC, 0, 0), 0, 0},
 	{NewNote(NaturalG, 0, 0), 1, 0},
 	{NewNote(NaturalD, 0, 0), 2, 0},
 	{NewNote(NaturalA, 0, 0), 3, 0},
 	{NewNote(NaturalE, 0, 0), 4, 0},
 	{NewNote(NaturalB, 0, 0), 5, 0},
+	{NewNote(NaturalG, 0, 1), 0, 6},
 	{NewNote(NaturalF, 1, 0), 6, 0},
+	{NewNote(NaturalD, 0, 1), 0, 5},
 	{NewNote(NaturalC, 1, 0), 7, 0},
+	{NewNote(NaturalA, 0, 1), 0, 4},
+	{NewNote(NaturalE, 0, 1), 0, 3},
+	{NewNote(NaturalB, 0, 1), 0, 2},
+	{NewNote(NaturalF, 0, 0), 0, 1},
 }
 
 var StringToKey = map[string]*Key{}
