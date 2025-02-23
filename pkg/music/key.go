38b9edc5dfe551ebcfc629eb010c7686a10bc065
Matt Fenwick <mfenwick100@gmail.com>
2025-02-22 19:35:13 -0500

    pull out constants

--- pkg/music/key.go
+++ pkg/music/key.go
@@ -15,21 +15,38 @@ func (k *Key) Signature() string {
 	return fmt.Sprintf("%s%s", strings.Repeat(SharpString, k.Sharps), strings.Repeat(FlatString, k.Flats))
 }
 
+var (
+	KeyC      = &Key{NewNote(NaturalC, 0, 0), 0, 0}
+	KeyCSharp = &Key{NewNote(NaturalC, 1, 0), 7, 0}
+	KeyDFlat  = &Key{NewNote(NaturalD, 0, 1), 0, 5}
+	KeyD      = &Key{NewNote(NaturalD, 0, 0), 2, 0}
+	KeyEFlat  = &Key{NewNote(NaturalE, 0, 1), 0, 3}
+	KeyE      = &Key{NewNote(NaturalE, 0, 0), 4, 0}
+	KeyF      = &Key{NewNote(NaturalF, 0, 0), 0, 1}
+	KeyFSharp = &Key{NewNote(NaturalF, 1, 0), 6, 0}
+	KeyGFlat  = &Key{NewNote(NaturalG, 0, 1), 0, 6}
+	KeyG      = &Key{NewNote(NaturalG, 0, 0), 1, 0}
+	KeyAFlat  = &Key{NewNote(NaturalA, 0, 1), 0, 4}
+	KeyA      = &Key{NewNote(NaturalA, 0, 0), 3, 0}
+	KeyBFlat  = &Key{NewNote(NaturalB, 0, 1), 0, 2}
+	KeyB      = &Key{NewNote(NaturalB, 0, 0), 5, 0}
+)
+
 var KeysByFifths = []*Key{
-	{NewNote(NaturalC, 0, 0), 0, 0},
-	{NewNote(NaturalG, 0, 0), 1, 0},
-	{NewNote(NaturalD, 0, 0), 2, 0},
-	{NewNote(NaturalA, 0, 0), 3, 0},
-	{NewNote(NaturalE, 0, 0), 4, 0},
-	{NewNote(NaturalB, 0, 0), 5, 0},
-	{NewNote(NaturalG, 0, 1), 0, 6},
-	{NewNote(NaturalF, 1, 0), 6, 0},
-	{NewNote(NaturalD, 0, 1), 0, 5},
-	{NewNote(NaturalC, 1, 0), 7, 0},
-	{NewNote(NaturalA, 0, 1), 0, 4},
-	{NewNote(NaturalE, 0, 1), 0, 3},
-	{NewNote(NaturalB, 0, 1), 0, 2},
-	{NewNote(NaturalF, 0, 0), 0, 1},
+	KeyC,
+	KeyG,
+	KeyD,
+	KeyA,
+	KeyE,
+	KeyB,
+	KeyGFlat,
+	KeyFSharp,
+	KeyDFlat,
+	KeyCSharp,
+	KeyAFlat,
+	KeyEFlat,
+	KeyBFlat,
+	KeyF,
 }
 
 var StringToKey = map[string]*Key{}
