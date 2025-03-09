2c97d0f3cff9431eedfd4e6f6ef1d85798de8ef0
Matt Fenwick <mfenwick100@gmail.com>
2025-03-09 18:07:15 -0400

    separate out node types

--- pkg/pratt/pratt.go
+++ pkg/pratt/pratt.go
@@ -1,9 +1,6 @@
 package pratt
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -89,57 +86,6 @@ func IsRightAssociative(op string, argCount int) bool {
 	default:
 		panic(errors.Errorf("invalid argCount for %s: %d", op, argCount))
 	}
-}
-
-type Node interface {
-	NodeString() string
-}
-
-func NodeString(node Node) string {
-	return strings.Join(nodeStringHelper(node, 0), "\n")
-}
-
-func nodeStringHelper(node Node, indent int) []string {
-	args := []string{}
-	val := ""
-	switch v := node.(type) {
-	case *OpNode:
-		val = v.Op
-		for _, arg := range v.Args {
-			args = append(args, nodeStringHelper(arg, indent+1)...)
-		}
-	case *NumNode:
-		val = v.Value
-	default:
-		panic(errors.Errorf("invalid node type: %+v", node))
-	}
-	return append([]string{fmt.Sprintf("%s%s", strings.Repeat("  ", indent), val)}, args...)
-}
-
-type NumNode struct {
-	Value string
-}
-
-func (n *NumNode) NodeString() string {
-	return n.Value
-}
-
-func Num(val string) *NumNode {
-	return &NumNode{Value: val}
-}
-
-type OpNode struct {
-	Op               string
-	ExpectedArgCount int
-	Args             []Node
-}
-
-func (o *OpNode) NodeString() string {
-	return o.Op
-}
-
-func Op(op string, expectedArgCount int, args ...Node) *OpNode {
-	return &OpNode{Op: op, ExpectedArgCount: expectedArgCount, Args: args}
 }
 
 func Parse(tokens []*Token) (Node, error) {
