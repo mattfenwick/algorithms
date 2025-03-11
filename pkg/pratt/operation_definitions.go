f878b3218417b933f6c102c175e401948ecfb23a
Matt Fenwick <mfenwick100@gmail.com>
2025-03-10 20:04:21 -0400

    split out operator definitions

---
+++ pkg/pratt/operation_definitions.go
@@ -0,0 +1,39 @@
+package pratt
+
+var TestOperators = NewOperators(
+	map[string]int{
+		"!":       100,
+		"+":       100,
+		"-":       100,
+		"^":       100,
+		"*":       100,
+		"&":       100,
+		"<-":      100,
+		"~":       100,
+		"pre-low": 0,
+		"pre-mid": 50,
+	},
+	map[string]int{
+		";":        100,
+		"post-low": 0,
+		"post-mid": 50,
+	},
+	[]*BinOp{
+		{"<-0", 0, false},
+		{"+", 40, false},
+		{"-", 40, false},
+		{"bin-mid-left", 50, false},
+		{"bin-mid-right", 50, true},
+		{"*", 60, false},
+		{"/", 60, false},
+		{"%", 60, false},
+		{"^", 70, false},
+		{"**", 70, true},
+	},
+	[]*TernOp{
+		{"?", ":", 0, true},
+		{"if", "else", 0, true},
+		{"<l", "<lb", 0, false},
+		{"<l1", "<l1b", 1, false},
+	},
+)
